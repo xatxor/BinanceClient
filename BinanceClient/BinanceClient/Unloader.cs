644838f7053a33dc65f4d5be8cd1ac64be6e1286
suborg <suborg@suborg.ru>
2020-07-25 23:15:40 +0300

    Вывод конкретной ошибки на экран в случае облома выгрузки

--- BinanceClient/BinanceClient/Unloader.cs
+++ BinanceClient/BinanceClient/Unloader.cs
@@ -13,11 +13,14 @@ namespace BinanceClient
         public void GetTradesAndRates(Binance.Net.BinanceClient client, string symbol, ref List<Tuple<DateTime, decimal>> trades, ref List<Tuple<DateTime, decimal>> rates, DateTime start, DateTime end)
         { 
             var aggTrades = client.GetAggregatedTrades(symbol, startTime: start, endTime: end, limit: 1000);
-            foreach (var t in aggTrades.Data)
-            {
-                trades.Add(new Tuple<DateTime, decimal>(t.TradeTime,t.Quantity));
-                rates.Add(new Tuple<DateTime, decimal>(t.TradeTime, t.Price));
-            }
+            if (aggTrades.Data != null)
+                foreach (var t in aggTrades.Data)
+                {
+                    trades.Add(new Tuple<DateTime, decimal>(t.TradeTime, t.Quantity));
+                    rates.Add(new Tuple<DateTime, decimal>(t.TradeTime, t.Price));
+                }
+            else
+                throw new Exception(aggTrades.Error.Message);
         }
 
     }
